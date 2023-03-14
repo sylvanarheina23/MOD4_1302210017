dd4bc49098e7f011008415942a85bfd4eb75a03f
unknown <sylvana.freelance@gmail.com>
2023-03-14 16:37:35 +0700

    menambahkan class kodebuah

---
+++ MOD4_1302210017/MOD4_1302210017/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MOD4_1302210017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
